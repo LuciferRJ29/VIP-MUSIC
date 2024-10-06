69c1417a495c9a173e3c4a6577dbb2953e1d3473
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-06 12:36:10 +0530

    Update play.py

--- VIPMUSIC/utils/inline/play.py
+++ VIPMUSIC/utils/inline/play.py
@@ -77,7 +77,7 @@ def stream_markup_timerr(_, videoid, chat_id, played, dur):
     buttons = [
         [
             InlineKeyboardButton(
-                text=f"{bar}",
+                text=f"{played} •{bar}• {dur}",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
